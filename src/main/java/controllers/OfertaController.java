18b932cadb7e68c64a468da662ab5aeb351f00b8
Lilith Nix <andromeda0209@gmail.com>
2023-10-10 19:49:36 -0300

    primer commit

--- src/main/java/controllers/OfertaController.java
+++ src/main/java/controllers/OfertaController.java
@@ -1,29 +1,14 @@
-/*package controllers;
+package controllers;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@RestController
 public class OfertaController {
-    public String createOferta(String unaDescripcion, int unStock, int unCodigoOferta, Float unPrecio) {
-        return "Oferta creada";
+
+    @GetMapping("/")
+    public String inicio() {
+        return "Oferta";
     }
-
-    SessionFactory sessionFactory = new Configuration().configure("Hibernate.cgf.xml").addAnnotationClass(Oferta.class).buildSessionFactory();
-
-    Session session = sessionFactory.openSession();
-
-    try {
-        Oferta oferta = new Oferta(unaDescripcion, unStock, unCodigoOferta, unPrecio);
-        session.beginTransaction();
-        session.save(oferta);
-        session.getTransaction().commit(); //efectuar cambio.
-        sessionFactory.close(); //cierra o detiene todo.
-        
-        return "Oferta creada";
-
-    } catch (Exception e) {
-
-        e.printStackTrace();
-    }
-}*/
+    
+}
